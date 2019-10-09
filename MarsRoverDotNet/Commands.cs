7beccd4734e0c6d4895f4cb3a5dad77ca0c5121e
Cameron Presley <Cameron@TheSoftwareMentor.com>
2019-10-08 23:12:16 -0400

    Added actions for the rover.

--- MarsRoverDotNet/Commands.cs
+++ MarsRoverDotNet/Commands.cs
@@ -6,55 +6,71 @@ namespace MarsRoverDotNet
     {
         public static Rover MoveForward(Rover r)
         {
-            switch(r.Direction)
+            switch (r.Direction)
             {
                 case Direction.North:
+                    return r.WithYOf(r.Y + 1);
                 case Direction.South:
+                    return r.WithYOf(r.Y - 1);
                 case Direction.East:
+                    return r.WithXOf(r.X + 1);
                 case Direction.West:
+                    return r.WithXOf(r.X - 1);
                 default:
-                return r;
+                    return r;
             }
         }
         public static Rover MoveBackward(Rover r)
         {
-switch(r.Direction)
+            switch (r.Direction)
             {
                 case Direction.North:
+                    return r.WithYOf(r.Y - 1);
                 case Direction.South:
+                    return r.WithYOf(r.Y + 1);
                 case Direction.East:
+                    return r.WithXOf(r.X - 1);
                 case Direction.West:
+                    return r.WithXOf(r.X + 1);
                 default:
-                return r;
+                    return r;
             }
         }
         public static Rover TurnLeft(Rover r)
         {
-switch(r.Direction)
+            switch (r.Direction)
             {
                 case Direction.North:
+                    return r.WithDirectionOf(Direction.West);
                 case Direction.South:
+                    return r.WithDirectionOf(Direction.East);
                 case Direction.East:
+                    return r.WithDirectionOf(Direction.North);
                 case Direction.West:
+                    return r.WithDirectionOf(Direction.South);
                 default:
-                return r;
+                    return r;
             }
         }
         public static Rover TurnRight(Rover r)
         {
-switch(r.Direction)
+            switch (r.Direction)
             {
                 case Direction.North:
+                    return r.WithDirectionOf(Direction.East);
                 case Direction.South:
+                    return r.WithDirectionOf(Direction.West);
                 case Direction.East:
+                    return r.WithDirectionOf(Direction.South);
                 case Direction.West:
+                    return r.WithDirectionOf(Direction.North);
                 default:
-                return r;
+                    return r;
             }
         }
 
-        public static Rover Quit (Rover r) => r;
-        public static Rover Unknown (Rover r) => r;
+        public static Rover Quit(Rover r) => r;
+        public static Rover Unknown(Rover r) => r;
 
     }
 }
