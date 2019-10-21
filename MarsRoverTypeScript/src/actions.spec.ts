8a9f546dac7c21b19de528e3410f63ef39408655
Cameron Presley <Cameron@TheSoftwareMentor.com>
2019-10-20 22:15:36 -0400

    Added tests when moving backward

--- MarsRoverTypeScript/src/actions.spec.ts
+++ MarsRoverTypeScript/src/actions.spec.ts
@@ -1,4 +1,4 @@
-import { moveForward } from "./actions";
+import { moveForward, moveBackward } from "./actions";
 import { Rover } from "./models";
 
 describe("When moving forward", () => {
@@ -27,6 +27,37 @@ describe("When moving forward", () => {
     const r: Rover = { x: 0, y: 0, direction: "West" };
     const newR = moveForward(r);
     expect(newR.x).toBe(r.x - 1);
+    expect(newR.direction).toBe(r.direction);
+    expect(newR.y).toBe(r.y);
+  });
+});
+
+describe("When moving backward", () => {
+  it("and facing north, then y decreases by one", () => {
+    const r: Rover = { x: 0, y: 0, direction: "North" };
+    const newR = moveBackward(r);
+    expect(newR.x).toBe(r.x);
+    expect(newR.direction).toBe(r.direction);
+    expect(newR.y).toBe(r.y - 1);
+  });
+  it("and facing south, then y increases by one", () => {
+    const r: Rover = { x: 0, y: 0, direction: "South" };
+    const newR = moveBackward(r);
+    expect(newR.x).toBe(r.x);
+    expect(newR.direction).toBe(r.direction);
+    expect(newR.y).toBe(r.y + 1);
+  });
+  it("and facing east, then x decreases by one", () => {
+    const r: Rover = { x: 0, y: 0, direction: "East" };
+    const newR = moveBackward(r);
+    expect(newR.x).toBe(r.x - 1);
+    expect(newR.direction).toBe(r.direction);
+    expect(newR.y).toBe(r.y);
+  });
+  it("and facing west, then x increases by one", () => {
+    const r: Rover = { x: 0, y: 0, direction: "West" };
+    const newR = moveBackward(r);
+    expect(newR.x).toBe(r.x + 1);
     expect(newR.direction).toBe(r.direction);
     expect(newR.y).toBe(r.y);
   });
