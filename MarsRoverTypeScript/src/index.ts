4ec232d67e2f1499e8196a99d78d846c3a420d28
Cameron Presley <Cameron@TheSoftwareMentor.com>
2021-07-18 20:12:30 -0400

    Removed readline package to use sync-prompt instead

--- MarsRoverTypeScript/src/index.ts
+++ MarsRoverTypeScript/src/index.ts
@@ -1,86 +1,79 @@
-import { createInterface } from "readline";
-const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
 import { Command, commandToAction } from "./commands";
 import { compose } from "./functional";
 import { Rover } from "./models";
 
+const prompt = require("prompt-sync")({sigint: true});
+
 const stringToCommand = (s: string): Command => {
-    switch (s.toLowerCase()) {
-        case "f": return "MoveForward";
-        case "b": return "MoveBackward";
-        case "l": return "TurnLeft";
-        case "r": return "TurnRight";
-        case "q": return "Quit";
-        default: return "Unknown";
-    }
+  switch (s.toLowerCase()) {
+    case "f":
+      return "MoveForward";
+    case "b":
+      return "MoveBackward";
+    case "l":
+      return "TurnLeft";
+    case "r":
+      return "TurnRight";
+    case "q":
+      return "Quit";
+    default:
+      return "Unknown";
+  }
 };
 
 let command: Command;
 
 const displayPrompt = (r: Rover) => {
-    console.log(r);
-    console.log("What command to run?");
-    console.log("Move (F)orward, Move (B)ackward, Turn (L)eft, Turn (R)ight, (Q)uit");
+  console.log(r);
+  console.log("What command to run?");
+  console.log(
+    "Move (F)orward, Move (B)ackward, Turn (L)eft, Turn (R)ight, (Q)uit"
+  );
 };
 
-console.log("How do you want to run the simulator?");
-console.log("1). Interactive (default)");
-console.log("2). String of Commands (no intermediate steps shown)");
-readline.prompt();
-readline.on("line", (line: string) => {
-    const rover: Rover = { x: 0, y: 0, direction: "North" };
-    if (!line || line === "1") {
-        runInteractive(rover);
-    } else {
-        runStringOfCommands(rover);
-    }
-});
+const main = () => {
+  console.log("How do you want to run the simulator?");
+  console.log("1). Interactive (default)");
+  console.log("2). String of Commands (no intermediate steps shown)");
+  let line = prompt();
+  const rover: Rover = { x: 0, y: 0, direction: "North" };
+  if (!line || line === "1") {
+    runInteractive(rover);
+  } else {
+    runStringOfCommands(rover);
+  }
+};
 
 const runInteractive = (r: Rover) => {
-    displayPrompt(r);
-    readline.prompt();
-    readline.on("line", (line: string) => {
-        command = stringToCommand(line);
-        r = commandToAction(command)(r);
-        if (command === "Quit") {
-            readline.close();
-        } else {
-            displayPrompt(r);
-            readline.prompt();
-        }
-    }).on("close", () => {
-        process.exit(0);
-    });
+  displayPrompt(r);
+  const line = prompt();
+  command = stringToCommand(line);
+  r = commandToAction(command)(r);
+  if (command === "Quit") {
+    process.exit();
+  } else {
+    runInteractive(r);
+  }
 };
 
 const runStringOfCommands = (rover: Rover) => {
-    console.log("What are the commands to process?");
-    readline.prompt();
-    readline.on("line", (line: string) => {
-        let newR: Rover;
-        // worst way (3 iterations)
-        newR = [...line]
-            .map(stringToCommand)
-            .map(commandToAction)
-            .reduce((r, f) => f(r), rover);
+  console.log("What are the commands to process?");
+  const line = prompt();
+  let newR: Rover;
+  // worst way (3 iterations)
+  newR = [...line]
+    .map(stringToCommand)
+    .map(commandToAction)
+    .reduce((r, f) => f(r), rover);
 
-        // Better way (2 iterations)
-        const stringToAction = compose(stringToCommand, commandToAction);
-        newR = [...line]
-            .map(stringToAction)
-            .reduce((r, f) => f(r), rover);
+  // Better way (2 iterations)
+  const stringToAction = compose(stringToCommand, commandToAction);
+  newR = [...line].map(stringToAction).reduce((r, f) => f(r), rover);
 
-        // Best way (1 iteration)
-        newR = [...line].reduce((r, c) => stringToAction(c)(r), rover);
+  // Best way (1 iteration)
+  newR = [...line].reduce((r, c) => stringToAction(c)(r), rover);
 
-        console.log(newR);
-        readline.close();
-    })
-        .on("close", () => {
-            process.exit(0);
-        });
+  console.log(newR);
 };
+
+main();
